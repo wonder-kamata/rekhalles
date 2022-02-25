4cc7a3df6bed21590f8b0024fe2457a385d457c3
wonder-kamata <wdkamata_staff@litalico.co.jp>
2022-02-25 19:17:33 +0900

    changed java.js and wrote rate.html
    added rate system to rate.html and added some codes into Java.js

--- Java.js
+++ Java.js
@@ -1,16 +1,39 @@
 
 const countUp = () => {
-  let rate = Math.random()
+  let rate
+  rate = Math.random()
   rate += 0.5
+  rate = Math.round(rate*10)/10
   return rate
 }
 
-let ugoki = countUp();
+const ex = () => {
+  let example
+  example = Math.round(123.456)
+  return example
+}
+// let ugoki = countUp();
 
-setInterval(() => {
-  const rate = countUp();
-  console.log(rate);
-}, 1000);
+// setInterval(() => {
+//   const rate = countUp();
+//   console.log(rate);
+// }, 1000);
 
-ugoki = countUp();
-console.log(ugoki);
+// ugoki = countUp();
+// console.log(ugoki);
+
+window.onload=function(){
+  var date=new Date();
+
+  document.getElementById("dollars").innerHTML = ex();
+
+  setInterval(() => {
+    const rate = countUp();
+    console.log(rate);
+    var date=new Date();
+    // document.getElementById("dollars").innerHTML = rate;
+    document.getElementById("dollars").innerHTML = rate;
+  }, 1000);
+
+// document.getElementById("dollars").innerHTML = date.getDate();
+}
